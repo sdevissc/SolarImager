e0f32add2edc9de0e7c4eafc4f13b2a31d85c490
sdevissc <simon.devisscer@gmail.com>
2026-04-01 20:51:22 +0200

    remove build

--- build/CMakeFiles/BaslerCameraLib_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "BaslerCameraLib_autogen/timestamp"
-  "CMakeFiles/BaslerCameraLib_autogen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/BaslerCameraLib_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/PlayerOneCameraLib_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PlayerOneCameraLib_autogen"
-  "PlayerOneCameraLib_autogen/timestamp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PlayerOneCameraLib_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
